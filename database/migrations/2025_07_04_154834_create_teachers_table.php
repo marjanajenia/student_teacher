e9f775e56fbb39810beeb57cb19d89093c1bb86d
marjanajenia <marjanajenia98@gmail.com>
2025-07-04 21:54:20 +0600

    Teacher dataTable create

---
+++ database/migrations/2025_07_04_154834_create_teachers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('password')->nullable();
+            $table->string('gender');
+            $table->string('address');
+            $table->string('phone');
+            $table->date('dob');
+            $table->date('date_of_join');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
